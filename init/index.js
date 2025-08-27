00ef992d984364b048dc995f61a0fea91751b6ce
Sujal Pachghare <sujalpachghare99@gmail.com>
2025-08-28 00:04:56 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(() => {
+    console.log("Connection is running");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL)
+};
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = 
+    initData.data.map((obj) => ({
+         ...obj,
+          owner: "685fc4227fc25bde21fcd2c2"
+     }));
+
+    await Listing.insertMany(initData.data);
+
+    console.log("Data was initialized successfully!");
+};
+
+
+initDB();
